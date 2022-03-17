58cf8f9922d3b4a712fc42a223b3e775c498da2c
0standing2 <ysyoung1011@sookmyung.ac.kr>
2022-03-18 02:43:26 +0900

    충돌..

--- src/routes/board.ts
+++ src/routes/board.ts
@@ -6,20 +6,10 @@ const router = express.Router();
 
 router.post("/imageUpload", upload.single('imgs'), boardController.imageUpload);
 router.post("/write", boardController.write);
-<<<<<<< HEAD
-<<<<<<< HEAD
 // router.post("/checkPw", boardController.checkPw);
 router.post("/checkBoardPermission", boardController.checkBoardPermission);
 router.delete("/delete" , boardController.deleteBoard);
 router.post("/update", boardController.update);
-=======
-<<<<<<< HEAD
-=======
->>>>>>> e7eb2c7 (충돌 해결)
-router.post("/checkBoardPermission", boardController.checkBoardPermission);
-router.delete("/delete/" , boardController.deleteBoard);
-router.post("/update/:id", boardController.update);
->>>>>>> c7c75fe (충돌 해결)
 router.get("/show/:id", boardController.showBoard);
 router.get("/list", boardController.list);
 router.get("/list/:category", boardController.listByCategory);
