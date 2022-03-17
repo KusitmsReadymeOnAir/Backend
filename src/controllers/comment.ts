7902867a69a611eac13889e21724a002353c6cfe
0standing2 <ysyoung1011@sookmyung.ac.kr>
2022-03-17 18:51:24 +0900

    충돌 해결

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -26,11 +26,7 @@ const addComment = async (req: Request, res: Response, next: NextFunction) => {
         userId : req.body.userId,
         createdAt:req.body.createdAt,
         parentComment : req.body.parentComment,
-<<<<<<< HEAD
         comment: req.body.Comment
-=======
-        comment: req.body.comment
->>>>>>> f5fccb3 (임시 커밋)
     });
 
     try {
@@ -74,21 +70,6 @@ const deleteComment = async( req : Request, res : Response, next : NextFunction)
     const { userId, commentId } = req.body;
 
     try {
-<<<<<<< HEAD
-        // const pwData = await Comment.findById({"_id":ObjectId(req.body.id)});
-        // console.log(pwData);
-        // if(pwData!.pw==req.body.pw){
-            await Comment.findByIdAndDelete({"_id":ObjectId(req.body.id)});
-            res.status(200).json({
-                result: "삭제 완료"
-            })
-        //}
-        // else{
-        //     res.status(500).json({
-        //         error: "비밀번호 불일치"
-        //     })
-        // }
-=======
         const data = await Comment.findById(ObjectId(commentId));
         console.log(data);
         if (!data) {
@@ -130,39 +111,10 @@ const checkCommentPermission = async( req : Request, res : Response, next : Next
         else {
             return true;
         }
->>>>>>> f5fccb3 (임시 커밋)
     }
     catch (error: any) {
         res.status(500).json({
             error: error.message
-        })
-    }
-}
-
-const checkCommentPermission = async( req : Request, res : Response, next : NextFunction) => {
-    try {
-        const { userId, commentId } = req.body;
-        const data = await Comment.findById(commentId);
-        console.log("commentId로 조회" ,data);
-        if(!data) { 
-            res.status(400).json({
-                error : "잘못된 댓글 번호입니다."
-            })
-        }
-        else if(data.userId != ObjectId(userId)) {
-            res.status(200).json({
-                data : data
-            })
-        }
-        else {
-            res.status(401).json({
-                error : "작성자가 아닙니다."
-            })
-        }
-    }
-    catch(error : any) {
-        res.status(500).json({
-            error : error.message
         })
     }
 }
